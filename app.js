d12040818633b919f5e113348dca8d27bfde4a6e
justcodingxdd <pateljainam25@gmail.com>
2023-06-17 23:14:13 +0530

    mongoDB

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const express= require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
-
-var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://0.0.0.0:27017/todolistDB");
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to the todo list!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add the new item"
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+})
+
+const defaultItems = [item1, item2, item3];
+
+
 
 app.get("/", function(req,res){
     
@@ -22,7 +45,27 @@ app.get("/", function(req,res){
 
     var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+async function getItems(){
+    const Items = await Item.find({});
+     return Items;
+}
+      
+app.get("/", function (req, res) {
+      
+    var today = new Date();
+      
+    var options = { weekday: "long", day: "numeric", year: "numeric" , month: "numeric" };
+      
+    var day = today.toLocaleDateString("en-GB", options);
+      
+    getItems().then(function(FoundItems){
+          
+    res.render("list", {kindOfDay: day, newItem:FoundItems});
+      
+    });
+      
+    });
+
 });
 
 app.post("/", function(req, res){
